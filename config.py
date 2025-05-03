574459dcf5018dd5d0281de1d019bff6a1da40cc
Stil-sh <denios228s@gmail.com>
2025-05-04 02:50:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "7954452949:AAFPjobmKF43QWu6oFC2szX_xTvoc9uClkk"
-DANBOORU_API_KEY = "https://danbooru.donmai.us/profile.json?api_key=your_api_key_goes_here&login=Stil_2005"  # Опционально
-DANBOORU_USER = "Stil_2005"  # Опционально
+DANBOORU_API_KEY = https://danbooru.donmai.us/profile.json?api_key=your_api_key_goes_here&login=Stil_2005  # Опционально
+DANBOORU_USER = Stil_2005  # Опционально
